86aae453921d70475c26c072c17ca5ac8409ed33
oldhorizons <newbrainspace@gmail.com>
2024-05-29 12:26:03 +1000

    added debug configurations

--- Code/kilosort/run_kilosort.py
+++ Code/kilosort/run_kilosort.py
@@ -1,6 +1,9 @@
 from kilosort.utils import download_probes
 from kilosort import run_kilosort, DEFAULT_SETTINGS
 
+#CONFIG
+data_dir = 'B:/SpikeData/JCPM4853/CROPPED'
+n_channels = 384
 
 # download channel maps for probes
 download_probes()
@@ -8,7 +11,7 @@ download_probes()
 #run kilosort
 settings = DEFAULT_SETTINGS
 # ( path to drive if mounted: /content/drive/MyDrive/ )
-settings['data_dir'] = 'B:/SpikeData/JCPM4853/CROPPED'
-settings['n_chan_bin'] = 384
+settings['data_dir'] = data_dir
+settings['n_chan_bin'] = n_channels
 
 ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = run_kilosort(settings=settings, probe_name='neuropixPhase3B1_kilosortChanMap.mat')
