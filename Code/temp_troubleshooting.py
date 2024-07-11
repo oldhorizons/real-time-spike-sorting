e89c48234f296460c2359548a7e64902efd21202
Michelle <newbrainspace@gmail.com>
2024-07-11 15:13:00 +1000

    fixed an issue with the troubleshooting page

--- Code/temp_troubleshooting.py
+++ Code/temp_troubleshooting.py
@@ -2,6 +2,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 os.chdir('C:/Users/miche/OneDrive/Documents/A-Uni/REIT4841/Data_Raw')
 dat1 = np.memmap('JCPM4853_CROPPED_6922ms/continuous.dat')
@@ -11,6 +12,66 @@ dat2 = np.memmap('HYBRID_JANELIA/continuous.dat')
 dat1 = dat1.reshape((len(dat1) // 384, 384))
 # dat2 = dat2.reshape((len(dat2) //16,16))
 
+def plot_heatmap(data, title, num_subplots = 5, ratio = 15, filename = None, save_img = False):
+    l = data.shape[0]
+    if data.shape[0] > data.shape[1]:
+        l = data.shape[1]
+    else:
+        # initial transpose because it's really hard to index this data otherwise
+        # also kilosort takes data in this format
+        #TODO though is this messing with anything? I'm assuming it just changes how np interprets indexes but I'm not 100%
+        data = np.transpose(data)
+    # ratio of about 1:15 channels:samples per row is good
+    l *= ratio
+    fig, axs = plt.subplots(num_subplots)
+    for i in range(0,num_subplots):
+        cropped = data[:][l*i:(l*i)+l]
+        cropped = np.transpose(cropped)
+        axs[i].imshow(cropped, cmap = 'hot', interpolation='nearest')
+        axs[i].set_xticklabels([])
+        axs[i].set_yticklabels([])
+    plt.suptitle(title)
+    plt.subplots_adjust(wspace=0, hspace=0)
+    if save_img:
+        if filename is None:
+            print("YOU FORGOT A FILENAME")
+            filename = f"{str(datetime.now())}.png"
+        plt.savefig("heatmaps/" + filename)
+    else:
+        plt.show()
+
+def show(data):
+    plt.imshow(data, cmap='hot') #interpolation = 'nearest'
+    plt.show()
+
+# show(dat2)
+
+original = np.memmap('HYBRID_JANELIA/continuous.dat')
+o1c = original.reshape(len(original) //16,16) #C-like index ordering
+o1f = original.reshape(len(original) // 16, 16, order='F') #Fortran-like index ordering
+o2c = original.reshape(16,len(original)//16)
+o2f = original.reshape(16,len(original)//16, order = 'F')
+
+(n1, ratio1) = (5, 20)
+(n2, ratio2) = (10, 40)
+save = True
+
+print("starting")
+plot_heatmap(dat1, "control: shape (samples, channels), C-like index ordering", n1, ratio1, f"CONTROL_n{n1}r{ratio1}.png", save)
+print("finished control")
+plot_heatmap(o1c, "1C: shape (samples, channels), C-like index ordering", n2, ratio2, f"1C_scC_n{n2}r{ratio2}.png", save)
+print("finished 1C")
+plot_heatmap(o1f, "1F: shape (samples, channels), Fortran-like index ordering", n2, ratio2, f"1F_scF_n{n2}r{ratio2}.png", save)
+print("finished 1F")
+plot_heatmap(o2c, "2C: shape (channels, samples), C-like index ordering", n2, ratio2, f"2C_csC_n{n2}r{ratio2}.png", save)
+print("finished 2C")
+plot_heatmap(o2f, "2F: shape (channels, samples), Fortran-like index ordering", n2, ratio2, f"2F_csF_n{n2}r{ratio2}.png", save)
+print("finished 2F")
+
+
+
+
+########################################################################
 
 def write_save(data, filename):
     with open(filename, 'wb') as f:
@@ -25,40 +86,6 @@ def save_cropped_np(data, name, offset=0, crop_rate = 1000, l = None):
     if not os.path.exists(directory):
         os.makedirs(directory)
     write_save(d, directory+"/continuous.dat")
-
-def plot_heatmap(data, title):
-    l = data.shape[0]
-    if data.shape[0] > data.shape[1]:
-        l = data.shape[1]
-    else:
-        data = np.transpose(data)
-    # ratio of about 1:15 channels:samples per row is good
-    l *= 30
-    fig, axs = plt.subplots(5)
-    for i in range(0,5):
-        cropped = data[:][l*i:(l*i)+l]
-        cropped = np.transpose(cropped)
-        axs[i].imshow(cropped, cmap = 'hot')
-    plt.title(title)
-    plt.show()
-
-def show(data):
-    plt.imshow(data, cmap='hot') #interpolation = 'nearest'
-    plt.show()
-
-# show(dat2)
-
-original = np.memmap('HYBRID_JANELIA/continuous.dat')
-o1a = original.reshape(len(original) //16,16) #C-like index ordering
-o1b = original.reshape(len(original) // 16, 16, order='F') #Fortran-like index ordering
-o2a = original.reshape(16,len(original)//16)
-o2b = original.reshape(16,len(original)//16, order = 'F')
-
-plot_heatmap(dat1, "control")
-plot_heatmap(o1a, "o1a")
-plot_heatmap(o1b, "o1b")
-plot_heatmap(o2a, "o2a")
-plot_heatmap(o2b, "o2b")
 
 # timestamps = np.load('timestamps.npy')
 # sample_numbers = np.load('sample_numbers.npy')
